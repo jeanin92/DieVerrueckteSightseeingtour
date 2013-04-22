f83b09f5ddc15c904cecb5f6f089eca80878524f
Christiane Helmchen <Jashi@gmx.com>
2013-04-22 12:46:56 +0200

    ADD: spielmodus_waehlen.rb

---
+++ features/step_definitions/spielmodus_waehlen.rb
@@ -0,0 +1,35 @@
+Angenommen(/^ich befinde mich auf dem Spielmodus\-Bildschirm$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Wenn(/^ich mich für ein Einspieler\-Spiel entscheide$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Dann(/^gelange ich zum Figurwahl\-Bildschirm$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Wenn(/^ich mich für ein Mehrspieler\-Spiel entscheide$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Dann(/^fragt mich das System auf wie vielen Geräten ich spielen möchte$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Wenn(/^ich sage, ich möchte auf einem Gerät spielen$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Dann(/^gelange ich zum Spieler\-Einfüge\-Bildschirm$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Wenn(/^ich sage, ich möchte auf mehreren Geräten spielen$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Dann(/^gelange ich zum Verbindungsaufbau\-Bildschirm$/) do
+    pending # express the regexp above with the code you wish you had
+end
