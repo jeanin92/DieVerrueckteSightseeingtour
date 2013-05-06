54f47d412724903ed7d6c56b154571c39e8fb292
Janina <janinaschilling@web.de>
2013-05-06 22:25:52 +0200

    FIX: build problem fixed

--- DVST/gen/com/dhbw/dvst/R.java
+++ DVST/gen/com/dhbw/dvst/R.java
@@ -28,6 +28,12 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int anleitung=0x7f030001;
+        public static final int modus=0x7f030002;
+        public static final int spielbildschirm=0x7f030003;
+        public static final int spieler=0x7f030004;
+        public static final int spielfigur=0x7f030005;
+        public static final int start=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
