d29345608719d9deb0e60c834429f92541f81b71
Christiane Helmchen <Jashi@gmx.com>
2013-05-10 09:55:57 +0200

    CHG: not working calabash ruby

--- DVST/features/spielmodus_waehlen.feature
+++ DVST/features/spielmodus_waehlen.feature
@@ -5,11 +5,12 @@ Funktionalität: Der Benutzer will den Spielmodus wählen.
 	Als Spieler
 	Will ich entscheiden, ob ich gegen den Computer oder andere Spieler spielen möchte und
 	ob ich auf einem oder mehreren Geräten spielen will.
+
 	
-	Szenario: Der Benutzer will gegen die KI spielen.
-		Angenommen ich befinde mich auf dem Spielmodus-Bildschirm
-		Wenn ich mich für ein Einspieler-Spiel entscheide
-		Dann gelange ich zum Figurwahl-Bildschirm
+	Szenario: Der Benutzer will gegen die KI spielen.	
+#		Angenommen ich befinde mich auf dem Spielmodus-Bildschirm
+#		Wenn ich mich für ein Einspieler-Spiel entscheide
+#		Dann gelange ich zum Figurwahl-Bildschirm
 	
 	Szenariogrundriss: Der Benutzer will gegen andere Spieler spielen.
 		Angenommen ich befinde mich auf dem Spielmodus-Bildschirm
--- DVST/features/step_definitions/spielmodus_waehlen.rb
+++ DVST/features/step_definitions/spielmodus_waehlen.rb
@@ -1,9 +1,11 @@
+require 'calabash-android/calabash_steps'
+
 Angenommen(/^ich befinde mich auf dem Spielmodus\-Bildschirm$/) do
-    pending # express the regexp above with the code you wish you had
+    performAction('assert_text', 'Spielmodus', true)
 end
 
 Wenn(/^ich mich für ein Einspieler\-Spiel entscheide$/) do
-    pending # express the regexp above with the code you wish you had
+    #pending # express the regexp above with the code you wish you had
 end
 
 Dann(/^gelange ich zum Figurwahl\-Bildschirm$/) do
@@ -15,21 +17,5 @@ Wenn(/^ich mich für ein Mehrspieler\-Spiel entscheide$/) do
 end
 
 Dann(/^fragt mich das System auf wie vielen Geräten ich spielen möchte$/) do
-    pending # express the regexp above with the code you wish you had
-end
-
-Wenn(/^ich sage, ich möchte auf einem Gerät spielen$/) do
-    pending # express the regexp above with the code you wish you had
-end
-
-Dann(/^gelange ich zum Spieler\-Einfüge\-Bildschirm$/) do
-    pending # express the regexp above with the code you wish you had
-end
-
-Wenn(/^ich sage, ich möchte auf mehreren Geräten spielen$/) do
-    pending # express the regexp above with the code you wish you had
-end
-
-Dann(/^gelange ich zum Verbindungsaufbau\-Bildschirm$/) do
     pending # express the regexp above with the code you wish you had
 end
