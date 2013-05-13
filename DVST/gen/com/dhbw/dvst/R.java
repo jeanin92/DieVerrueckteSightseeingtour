1aabca8ab3863f79f313ba3a7e9ec76511ef86b2
Christiane Helmchen <Jashi@gmx.com>
2013-05-13 23:45:00 +0200

    CHG: R.java

--- DVST/gen/com/dhbw/dvst/R.java
+++ DVST/gen/com/dhbw/dvst/R.java
@@ -20,20 +20,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f080012;
         public static final int anleitung=0x7f080000;
-        public static final int btn_anleitung=0x7f08000d;
-        public static final int btn_einspieler=0x7f080008;
-        public static final int btn_mehrspieler=0x7f080009;
-        public static final int btn_ok=0x7f080007;
-        public static final int btn_start=0x7f08000c;
+        public static final int btn_anleitung=0x7f080011;
+        public static final int btn_einspieler=0x7f08000a;
+        public static final int btn_mehrspieler=0x7f08000b;
+        public static final int btn_ok=0x7f080008;
+        public static final int btn_start=0x7f080010;
+        public static final int geraete=0x7f080003;
         public static final int hd_anleitung=0x7f080001;
-        public static final int hd_geraete=0x7f080003;
-        public static final int hd_start=0x7f08000b;
-        public static final int rb_eins=0x7f080005;
-        public static final int rb_mehrere=0x7f080006;
-        public static final int rg_geraete=0x7f080004;
-        public static final int start=0x7f08000a;
+        public static final int hd_geraete=0x7f080004;
+        public static final int hd_start=0x7f08000f;
+        public static final int modus=0x7f080009;
+        public static final int rb_eins=0x7f080006;
+        public static final int rb_mehrere=0x7f080007;
+        public static final int rg_geraete=0x7f080005;
+        public static final int spieler_uebersicht=0x7f08000c;
+        public static final int spielfigur=0x7f08000d;
+        public static final int start=0x7f08000e;
         public static final int tv_anleitung=0x7f080002;
     }
     public static final class layout {
