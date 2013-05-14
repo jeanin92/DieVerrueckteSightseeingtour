a94a2d1d280ed627ac3a28fde4040925c45fe330
Christiane Helmchen <Jashi@gmx.com>
2013-05-15 01:29:57 +0200

    CHG: add click on radio buttons for UC1

--- DVST/features/step_definitions/geraete.rb
+++ DVST/features/step_definitions/geraete.rb
@@ -1,15 +1,16 @@
 Dann(/^fragt mich das System auf wie vielen Geraeten ich spielen moechte$/) do
     query("* id:'geraete' button text:'Okay' isEnabled:true")
-    pending #radio buttons
+    #radio buttons
     performAction('assert_text', 'Geraeteanzahl', true)
 end
 
 Wenn(/^ich sage, ich moechte auf "(.*?)" spielen$/) do |arg1|
     if @arg1 == 'einem Geraet' then
-        pending #radio button 1 Geraet ausgewaehlt
+        #radio button 1 Geraet ausgewaehlt
+        choose('rb_eins')
         performAction('press_button_with_text', 'Okay')
     elsif @arg1 == 'mehreren Geraeten' then
-        pending #radio button mehrere Geraete ausgewaehlt
+        #pending #radio button mehrere Geraete ausgewaehlt
         performAction('press_button_with_text', 'Okay')
     end
 end
--- DVST/features/step_definitions/views.rb
+++ DVST/features/step_definitions/views.rb
@@ -3,7 +3,6 @@ Angenommen(/^ich befinde mich auf dem Spielmodus\-Bildschirm$/) do
     query("* id:'start'button text:'Anleitung lesen' isEnabled:true")
     performAction('assert_text', 'Startseite', true)
     performAction('press_button_with_text', 'Spiel starten')
-    #touch("button text='Spiel starten'")
     query("* id:'modus' button text:'Einspieler' isEnabled:false")
     query("* id:'modus' button text:'button text:'Mehrspieler' isEnabled:true")
     performAction('assert_text', 'Spielmodus auswaehlen', true)
