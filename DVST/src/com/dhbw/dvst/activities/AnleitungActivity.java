93f2c326478076f7935d76d949cde4cf0f7fbf7b
Janina <janinaschilling@web.de>
2013-05-15 11:23:31 +0200

    CHG: anleitung Activity angepasst

--- DVST/src/com/dhbw/dvst/activities/AnleitungActivity.java
+++ DVST/src/com/dhbw/dvst/activities/AnleitungActivity.java
@@ -1,9 +1,12 @@
 package com.dhbw.dvst.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import com.dhbw.dvst.R;
 
@@ -13,6 +16,15 @@ public class AnleitungActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.anleitung);
+		
+		final Button btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+//            	setContentView(R.layout.modus);
+                Intent intent_modus = new Intent(AnleitungActivity.this, ModusActivity.class);
+                AnleitungActivity.this.startActivity(intent_modus);
+            }
+        });
 	}
 
 	@Override
