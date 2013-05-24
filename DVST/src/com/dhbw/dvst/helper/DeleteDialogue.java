413356f661b726ef32e4e06154cf5e675d83ee36
Christiane Helmchen <Jashi@gmx.com>
2013-05-24 17:51:58 +0200

    CHG: refactor popups

--- DVST/src/com/dhbw/dvst/helper/DeleteDialogue.java
+++ DVST/src/com/dhbw/dvst/helper/DeleteDialogue.java
@@ -1,40 +1,22 @@
 package com.dhbw.dvst.helper;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 
-import com.dhbw.dvst.R;
-import com.dhbw.dvst.activities.SpielerActivity;
-import com.dhbw.dvst.activities.SpielerUebersichtActivity;
 import com.dhbw.dvst.model.Control;
 import com.dhbw.dvst.model.Spiel;
 import com.dhbw.dvst.model.Spieler;
 
-public class DeleteDialogue {
+public class DeleteDialogue extends PopupService {
 	private Spiel spiel = Control.getInstance();
 	private Spieler spieler;
 
 	public DeleteDialogue(Activity activity, String fehler, Spieler spieler) {
+		super(activity, fehler, spieler);
 		this.spieler = spieler;
-		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-		builder.setMessage(fehler)
-			.setPositiveButton(R.string.ja, new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					spiel.spielerLoeschen(DeleteDialogue.this.spieler);
-				}
-			})
-			.setNegativeButton(R.string.nein, new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();											
-				}
-			});
-		AlertDialog alert = builder.create();
-		alert.show();
 	}
 	
+	protected void klickePositivenButton() {
+		spiel.spielerLoeschen(DeleteDialogue.this.spieler);
+	}
 	
 }
--- DVST/src/com/dhbw/dvst/helper/SimpleErrorMessage.java
+++ DVST/src/com/dhbw/dvst/helper/SimpleErrorMessage.java
@@ -1,23 +1,10 @@
 package com.dhbw.dvst.helper;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 
-import com.dhbw.dvst.R;
-
-public class SimpleErrorMessage {
+public class SimpleErrorMessage extends PopupService{
 
 	public SimpleErrorMessage(Activity activity, String fehler) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-		builder.setMessage(fehler)
-			.setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();											
-				}
-			});
-		AlertDialog alert = builder.create();
-		alert.show();
+		super(activity, fehler);
 	}
 }
