94b8149f4124264b64a563e2765739342b15bcc8
Christiane Helmchen <Jashi@gmx.com>
2013-05-27 23:50:48 +0200

    DEL: old maven project

--- DVST_old/features/step_definitions/modus.rb
+++
@@ -1,7 +0,0 @@
-Wenn(/^ich mich fuer ein Einspieler\-Spiel entscheide$/) do
-    performAction('press_button_with_text', 'Einspieler')
-end
-
-Wenn(/^ich mich fuer ein Mehrspieler\-Spiel entscheide$/) do
-    performAction('press_button_with_text', 'Mehrspieler')
-end
