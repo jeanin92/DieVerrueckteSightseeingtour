415d32fac21c89e74e66474844fd2703e510dbaf
Christiane Helmchen <Jashi@gmx.com>
2013-05-28 13:16:24 +0200

    CHG: refactor startView

--- DVST/src/com/dhbw/dvst/activities/StartActivity.java
+++ DVST/src/com/dhbw/dvst/activities/StartActivity.java
@@ -1,56 +1,43 @@
 package com.dhbw.dvst.activities;
 
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-
 import com.dhbw.dvst.R;
 import com.dhbw.dvst.models.Control;
 import com.dhbw.dvst.utilities.ActivityInteraction;
+import com.dhbw.dvst.views.StartView;
 
 public class StartActivity extends Activity {
 	private ActivityInteraction kommunikation = new ActivityInteraction();
+	private StartView view;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start);
 		
-		final Button btn_start = (Button) findViewById(R.id.btn_start);
-        setStartButtonListener(btn_start);
-        
-        final Button btn_anleitung = (Button) findViewById(R.id.btn_anleitung);
-        setAnleitungButtonListener(btn_anleitung);
+		view = (StartView)View.inflate(this, R.layout.start, null);
+		view.setViewListener(viewListener);
+		setContentView(view);
 	}
 	
-	protected void setAnleitungButtonListener(final Button btn_anleitung) {
-		btn_anleitung.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	kommunikation.navigieren(StartActivity.this, AnleitungActivity.class);
-            }
-        });
-	}
+	/**
+	 * This is how we receive events from the view.(Funktionen, die im View im Template aufgerufen werden)
+	 * The view takes user actions
+	 * The controller/activity responds to user actions
+	 */
+	private StartView.ViewListener viewListener = new StartView.ViewListener() {
+		
+		@Override
+		public void onStartSettings() {
+        	Control.resetInstance();
+            kommunikation.navigieren(StartActivity.this, ModusActivity.class);
+		}
 
-	protected void setStartButtonListener(final Button btn_start) {
-		btn_start.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	Control.resetInstance();
-                kommunikation.navigieren(StartActivity.this, ModusActivity.class);
-            }
-        });
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		boolean result = super.onCreateOptionsMenu(menu);
-		return result;
-	}
-	
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-	  super.onConfigurationChanged(newConfig);
-	}
+		@Override
+		public void onReadManual() {
+			kommunikation.navigieren(StartActivity.this, AnleitungActivity.class);
+		}
+	};
 }
