dbc44ec0c7b0b2020b61566eccf7d5609e3a1698
Christiane Helmchen <Jashi@gmx.com>
2013-05-28 14:48:18 +0200

    CHG: refactor geraete layout

--- DVST/src/com/dhbw/dvst/activities/GeraeteActivity.java
+++ DVST/src/com/dhbw/dvst/activities/GeraeteActivity.java
@@ -1,59 +1,47 @@
 package com.dhbw.dvst.activities;
 
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
 
 import com.dhbw.dvst.R;
 import com.dhbw.dvst.models.Control;
 import com.dhbw.dvst.models.Spiel;
 import com.dhbw.dvst.utilities.ActivityInteraction;
+import com.dhbw.dvst.views.GeraeteView;
 
 public class GeraeteActivity extends Activity {
 	private Spiel spiel = Control.getInstance();
 	private ActivityInteraction kommunikation = new ActivityInteraction();
+	private GeraeteView view;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.geraete);
-		
-		final Button btn_ok = (Button) findViewById(R.id.btn_ok);
-		final RadioButton rb_eins = (RadioButton) findViewById(R.id.rb_eins);
-		final RadioButton rb_mehrere = (RadioButton) findViewById(R.id.rb_mehrere);
-        setOkButtonListener(btn_ok, rb_eins, rb_mehrere);
-	}
 
-	protected void setOkButtonListener(final Button btn_ok,
-		final RadioButton rb_eins, final RadioButton rb_mehrere) {
-			btn_ok.setOnClickListener(new View.OnClickListener() {
-				public void onClick(View v) {
-					if (rb_eins.isChecked()){
-						spiel.setSpielmodus(Spiel.modus_mehrspieler_client);
-					}
-					else if(rb_mehrere.isChecked()){
-						spiel.setSpielmodus(Spiel.modus_mehrspieler_server);
-					}
-					kommunikation.navigieren(GeraeteActivity.this, SpielerUebersichtActivity.class);
-				}
-			});
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		view = (GeraeteView)View.inflate(this, R.layout.geraete, null);
+		view.setViewListener(viewListener);
+		setContentView(view);
 	}
 	
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-	  // ignore orientation/keyboard change
-	  super.onConfigurationChanged(newConfig);
-	}
+	private GeraeteView.ViewListener viewListener = new GeraeteView.ViewListener() {
 
+		private int radioButton;
+
+		@Override
+		public void onGeraetezahlBestaetigen() {
+			if (this.radioButton == R.id.rb_eins) {
+				spiel.setSpielmodus(Spiel.modus_mehrspieler_client);
+			}
+			else if(this.radioButton == R.id.rb_mehrere) {
+				spiel.setSpielmodus(Spiel.modus_mehrspieler_server);
+			}
+			kommunikation.navigieren(GeraeteActivity.this, SpielerUebersichtActivity.class);
+		}
+
+		@Override
+		public void onGeraeteZahlAuswaehlen(int radioButton) {
+			this.radioButton = radioButton;
+		}
+	};
 }
