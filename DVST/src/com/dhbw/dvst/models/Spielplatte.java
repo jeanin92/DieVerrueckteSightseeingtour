70ee3529b51731a614ad370a15c765245e98c60b
Christiane Helmchen <Jashi@gmx.com>
2013-06-05 21:25:44 +0200

    ADD: Spielplatte move right and left

--- DVST/src/com/dhbw/dvst/models/Spielplatte.java
+++ DVST/src/com/dhbw/dvst/models/Spielplatte.java
@@ -2,4 +2,28 @@ package com.dhbw.dvst.models;
 
 public class Spielplatte {
 
+	private boolean obenOffen, rechtsOffen, untenOffen, linksOffen;
+	
+	public Spielplatte(boolean o, boolean r, boolean u, boolean l) {
+		this.obenOffen = o;
+		this.rechtsOffen = r;
+		this.untenOffen = u;
+		this.linksOffen = l;
+	}
+	
+	public void nachRechtsDrehen() {
+		boolean tempRechts = this.rechtsOffen;
+		this.rechtsOffen = this.obenOffen;
+		this.obenOffen = this.linksOffen;
+		this.linksOffen = this.untenOffen;
+		this.untenOffen = tempRechts;
+	}
+	
+	public void nachLinksDrehen() {
+		boolean tempOben = this.obenOffen;
+		this.obenOffen = this.rechtsOffen;
+		this.rechtsOffen = this.untenOffen;
+		this.untenOffen = this.linksOffen;
+		this.linksOffen = tempOben;
+	}
 }
