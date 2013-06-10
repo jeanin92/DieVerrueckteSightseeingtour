b09c9daca6f682e2a3ff2c3d9fe6eb47c18460e3
Christiane Helmchen <Jashi@gmx.com>
2013-06-11 00:57:28 +0200

    ADD: new SpielDialog

---
+++ DVST/src/com/dhbw/dvst/utilities/SpielDialog.java
@@ -0,0 +1,25 @@
+package com.dhbw.dvst.utilities;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
+import com.dhbw.dvst.R;
+
+public class SpielDialog {
+
+	public SpielDialog(Activity activity, String message, OnClickListener posListener) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+		builder.setMessage(message);
+		builder.setPositiveButton(R.string.ja, posListener);
+		builder.setNegativeButton(R.string.nein, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();											
+				}
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+}
