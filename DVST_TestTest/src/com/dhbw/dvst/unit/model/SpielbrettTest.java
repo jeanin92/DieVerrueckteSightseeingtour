59327c0ed5f58f458b0674d850ed84653bf19f7d
Janina <janinaschilling@web.de>
2013-06-10 18:17:14 +0200

    ADD: sonar config

--- DVST_TestTest/src/com/dhbw/dvst/unit/model/SpielbrettTest.java
+++ DVST_TestTest/src/com/dhbw/dvst/unit/model/SpielbrettTest.java
@@ -1,16 +1,25 @@
 package com.dhbw.dvst.unit.model;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import com.dhbw.dvst.models.Ausrichtung;
+import com.dhbw.dvst.models.Farbe;
+import com.dhbw.dvst.models.Form;
 import com.dhbw.dvst.models.Kreuzung;
 import com.dhbw.dvst.models.Kurve;
 import com.dhbw.dvst.models.Spielbrett;
+import com.dhbw.dvst.models.Spieler;
+import com.dhbw.dvst.models.Spielfigur;
+import com.dhbw.dvst.models.Spielplatte;
+
 import junit.framework.TestCase;
 
 public class SpielbrettTest extends TestCase{
 
 	private Spielbrett testBrett;
+	private ArrayList<Spieler> testSpieler = new ArrayList<Spieler>();
 
 	public SpielbrettTest(String name) {
 		super(name);
@@ -19,6 +28,8 @@ public class SpielbrettTest extends TestCase{
 	protected void setUp() throws Exception {
 		super.setUp();
 		testBrett = new Spielbrett();
+		testSpieler.add(new Spieler("test1", new Spielfigur(new Form("auto", "car"), new Farbe("rot", "red"), "car_red")));
+		testSpieler.add(new Spieler("test2", new Spielfigur(new Form("auto", "car"), new Farbe("blau", "blue"), "car_blue")));
 	}
 	
 	public void testObIntArrayKorrekteLaengeHat() {
@@ -60,6 +71,15 @@ public class SpielbrettTest extends TestCase{
 		testBrett.fuegeStatischePlattenEin();
 		assertEquals(new Kreuzung(Ausrichtung.KREUZUNGUNTEN).getAusrichtung(),
 				this.testBrett.getAlleSpielplatten().get(2).getAusrichtung());
+	}
+	
+	public void testeSpielfigurPlatziert(){
+		testBrett.baueIntArrayMitAnzahlVerschiedenerPlatten();
+		testBrett.fuelleLosesSpielplattenArray();
+		testBrett.fuegeStatischePlattenEin();
+		testBrett.verteileSpielfiguren(testSpieler);
+		assertNotNull(testSpieler.get(0).getSpielfigur().getSpielplatte());
+		assertNotNull(testSpieler.get(1).getSpielfigur().getSpielplatte());
 	}
 	
 	protected void tearDown() throws Exception {
